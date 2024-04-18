e0b6875d74021a5504656e09e69188df67d81888
yasmina <el.yasmina.08@gmail.com>
2024-04-18 12:39:04 +0200

    génération de la page de login et du background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'infinite_quiz';
+
+  constructor(private router: Router, private render: Renderer2){};
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateBodyStyle(event.url);
+      }
+    });
+  }
+// Impossible d'utiliser ngClass sur body car ce n'est pas un composant angular donc il n'est pas reconnu
+  updateBodyStyle(url: string): void {
+    if (url === '/login' || url === '/register') {
+      this.render.setStyle(document.body, 'background-color', '#8906E6');
+    } else {
+      this.render.removeStyle(document.body, 'background-color');
+    }
+  }
+
+
+  
 }
