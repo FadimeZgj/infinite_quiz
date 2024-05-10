edaea751971ed7eecb9f3e48c68ac6cab3c7d259
yasmina <el.yasmina.08@gmail.com>
2024-05-10 22:24:13 +0200

    ✨features: ajout du formulaire d'inscription comprenant le contrôle des entrées utilisateurs coté front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,23 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'infinite_quiz';
 
-  constructor(private router: Router, private render: Renderer2){};
+//   constructor(private router: Router, private render: Renderer2){};
 
-  ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.updateBodyStyle(event.url);
-      }
-    });
-  }
-// Impossible d'utiliser ngClass sur body car ce n'est pas un composant angular donc il n'est pas reconnu
-  updateBodyStyle(url: string): void {
-    if (url === '/login' || url === '/register') {
-      this.render.setStyle(document.body, 'background-color', '#8906E6');
-    } else {
-      this.render.removeStyle(document.body, 'background-color');
-    }
-  }
+//   ngOnInit(): void {
+//     this.router.events.subscribe(event => {
+//       if (event instanceof NavigationEnd) {
+//         this.updateBodyStyle(event.url);
+//       }
+//     });
+//   }
+// // Impossible d'utiliser ngClass sur body car ce n'est pas un composant angular donc il n'est pas reconnu
+//   updateBodyStyle(url: string): void {
+//     if (url === '/login' || url === '/register') {
+//       this.render.setStyle(document.body, 'background-color', '#8906E6');
+//     } else {
+//       this.render.removeStyle(document.body, 'background-color');
+//     }
+  // }
 
 
   
