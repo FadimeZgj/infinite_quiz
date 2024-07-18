8495da68b255ecf68bac67b4ff1f789c3691deed
yasmina <el.yasmina.08@gmail.com>
2024-07-18 20:26:22 +0200

    ✨ features : ajout du token dans le localStorage et du pot de miel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +12,39 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginComponent {
 
+  http : HttpClient =inject(HttpClient);
+  router: Router = inject(Router);
+  user_msg!: string;
+
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    pw:  new FormControl(''),
-    robbot: new FormControl(''),
+    username: new FormControl(''),
+    password:  new FormControl(''),
+    honneypot: new FormControl(''),
   });
 
   onSubmit() {
+    const form=this.loginForm.value
+    if (!form['honneypot']) {
+      
+      this.http.post<any>('http://127.0.0.1:8000/api/login_check',JSON.stringify(form), {headers: {'Content-Type': 'application/ld+json' }})
+      .subscribe({
+        next :  (resp) => {
+          console.log(resp);
+        if (resp) {
+          localStorage.setItem('jwt', resp.token);
+          this.router.navigateByUrl('/dashboard');
+        } 
+          
+      },
+
+      error: (err)=>{this.user_msg="l'email ou le mot de passe est incorrect"},
+      }
+
+    ) 
+    } else {
+      this.router.navigateByUrl('/login');
+    }
     console.log(this.loginForm.value);
   }
   
 }
-
