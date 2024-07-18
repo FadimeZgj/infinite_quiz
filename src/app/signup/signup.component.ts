afc223cd3f2316882eb1820fb915bd24b2658d5c
yasmina <el.yasmina.08@gmail.com>
2024-07-18 20:58:10 +0200

    ✨ features: ajout dans le signup de la gestion des erreurs et du pot de miel

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+
+import { HttpClient} from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,36 +12,57 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-  
-  noMatch : boolean = false
+
+  http : HttpClient =inject(HttpClient);
+  router : Router = inject(Router);
+  error_msg:string="";
 
   signUpForm = new FormGroup({
     firstname: new FormControl('', [Validators.required, ]),
     email: new FormControl('',[Validators.required, Validators.email]),
-    pw:  new FormControl('', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/)]),
+    plainPassword:  new FormControl('', [Validators.required]),
+    // plainPassword:  new FormControl('', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*+_'-]).{12,}$/)]),
     cpw: new FormControl('', [Validators.required, ]),
-    robbot: new FormControl(''),
+    honneypot: new FormControl(''),
   });
 
   onSubmit() {
-    let formInputs=this.signUpForm.value
-    if (formInputs['pw']!=formInputs['cpw']) {
-      return this.noMatch = true;
+    const formInputs=this.signUpForm.value
+    if (formInputs['plainPassword']!=formInputs['cpw']) {
+      return this.error_msg="La confirmation ne correspond pas au mot de passe entré."
     } else if(this.signUpForm.invalid)
       {
-        return false
+        return this.error_msg="La confirmation ne correspond pas au mot de passe entré."
       }
-      else if (formInputs['robbot']?.length !=0) {
-        window.location.reload();
+      else if (formInputs['honneypot']?.length !=0) {
+        this.error_msg="Le formulaire n'est pas conforme. Veuillez réessayer."
         return false;
       }
     
     else{
-      console.log(this.signUpForm.value);
+
+      delete formInputs['cpw']
+      
+      const user={username:formInputs['email'], password:formInputs['plainPassword'], honneypot:formInputs['honneypot']}
+
+      this.http.post<any>('http://127.0.0.1:8000/api/users',JSON.stringify(this.signUpForm.value), {headers: {'Content-Type': 'application/ld+json' }}).subscribe({
+        next: (resp) =>{
+       
+        console.log("inscrit");
+              
+      },
+      error: (err)=>{this.error_msg="Le formulaire n'est pas conforme. Veuillez réessayer."}})
+
+      this.http.post<any>('http://127.0.0.1:8000/api/login_check',JSON.stringify(user), {headers: {'Content-Type': 'application/ld+json' }}).subscribe({
+        next: (resp) =>{
+        localStorage.setItem('jwt', resp.token);
+       this.router.navigateByUrl('/dashboard');
+        
+      }, error: (err)=>{this.error_msg="Le formulaire n'est pas conforme. Veuillez réessayer."}})
+      
     }
 
     return false
-
 
 
     // invalid permet de vérifier s'il y a eu au moins une erreur dans le formGroup
@@ -46,8 +70,6 @@ export class SignupComponent {
     //le ? c pour que Typsecript ne mette pas une erreur car au départ l'erreur est null et ['pattern'] fait référence au validator plus haut
     // console.log(this.signUpForm.controls.pw.errors?.['pattern']);
     
-    // console.log(this.signUpForm.value);
-    // console.log(this.signUpForm);
 
   }
 
