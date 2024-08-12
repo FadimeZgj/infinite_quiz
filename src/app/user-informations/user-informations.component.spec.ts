9ae02c5861fe6da34e7c2be9163e950b7da22456
yasmina <el.yasmina.08@gmail.com>
2024-08-12 21:00:58 +0200

    🚧 ajout de loader, du controle des données et modification des tests en conséquence

--- src/app/user-informations/user-informations.component.spec.ts
+++ src/app/user-informations/user-informations.component.spec.ts
@@ -1,197 +1,148 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, Router } from '@angular/router';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { UserInformationsComponent } from './user-informations.component';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { JwtService } from '../services/jwtServices/jwt.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CleanDataService } from '../services/cleanDataService/clean-data.service';
+import { LoaderService } from '../services/loaderService/loader.service';
+import * as CryptoJS from 'crypto-js';
+import { ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
 
 describe('UserInformationsComponent', () => {
   let component: UserInformationsComponent;
   let fixture: ComponentFixture<UserInformationsComponent>;
-  let router: Router;
-  let httpTestingController: HttpTestingController;
-  let jwtService: JwtService;
+  let cleanDataServiceSpy: jasmine.SpyObj<CleanDataService>;
+  let loaderServiceSpy: jasmine.SpyObj<LoaderService>;
 
   beforeEach(async () => {
+    cleanDataServiceSpy = jasmine.createSpyObj('CleanDataService', ['cleanObject']);
+    loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['show', 'hide']);
+
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, UserInformationsComponent],
       providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: {} } },
-        JwtService
+        { provide: CleanDataService, useValue: cleanDataServiceSpy },
+        { provide: LoaderService, useValue: loaderServiceSpy },
+        { provide: ActivatedRoute, useValue: { snapshot: {} } } 
       ],
     }).compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserInformationsComponent);
     component = fixture.componentInstance;
-    httpTestingController = TestBed.inject(HttpTestingController);
-    router = TestBed.inject(Router);
-    spyOn(router, 'navigateByUrl');
-    jwtService = TestBed.inject(JwtService);
-    // spyOn(jwtService, 'decode').and.returnValue({ username: 'john@doe.fr' });
-
-    // Configuration de localStorage pour chaque test
-    // localStorage.setItem('jwt', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU');
+    fixture.detectChanges();
   });
 
-  afterEach(() => {
-    httpTestingController.verify(); // Vérifie que toutes les requêtes ont été traitées
-    localStorage.removeItem('jwt'); // Réinitialise localStorage après chaque test
-  });
-
-  it('should create', () => {
-    fixture.detectChanges(); // Assurez-vous que ngOnInit est appelé ici
+  it('devrait créer le composant', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should navigate to login if JWT is missing or invalid', () => {
-    localStorage.setItem('jwt', ''); // Simule un JWT invalide
-    fixture.detectChanges(); // Appelle ngOnInit
-    expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
+  it("devrait initialiser les informations utilisateur dans ngOnInit et les afficher dans les champs du formulaire", () => {
+    const userInfos = {
+      id: '123',
+      email: 'john@doe.fr',
+      firstname: 'John'
+    };
+    component.jwt = 'valid.jwt.token';
+    spyOn(sessionStorage, 'getItem').and.returnValue(CryptoJS.AES.encrypt(JSON.stringify(userInfos), CryptoJS.SHA256(component.jwt).toString()).toString());
+    
+    component.ngOnInit();
+  
+    expect(component.userId).toBe(userInfos.id);
+    expect(component.user_email).toBe(userInfos.email);
+    expect(component.user_name).toBe(userInfos.firstname);
+    expect(component.userForm.value.firstname).toBe(userInfos.firstname);
+    expect(component.userForm.value.email).toBe(userInfos.email);
+    expect(loaderServiceSpy.hide).toHaveBeenCalled();
   });
 
-  it('should submit the form successfully and display a success message', () => {
-    const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU'; // JWT factice
-    localStorage.setItem('jwt', fakeJwt);
-    component.userId = '123';
-    component.user_name = 'John';
-    component.user_email = 'john@doe.fr'
-    
-    component.userForm.setValue({ firstname: 'John', email: '', honneypot: '' });
-    
-    component.onSubmit();
+  it("devrait faire une redirection vers le login s'il le jwt est abscent", () => {
+    spyOn(component.router, 'navigateByUrl');
   
-    const req = httpTestingController.expectOne(`http://127.0.0.1:8000/api/users/123`);
-    expect(req.request.method).toBe('PATCH');
-    expect(req.request.body).toEqual(JSON.stringify({
+    component.jwt = '';
+    component.ngOnInit();
+  
+    expect(loaderServiceSpy.hide).toHaveBeenCalled();
+    expect(component.router.navigateByUrl).toHaveBeenCalledWith('/login');
+  });
+
+  it("ne devrait pas soummettre le formulaire si honneypot n'est pas vide", () => {
+    component.userId = '123';
+    component.jwt = 'valid.jwt.token';
+
+    const formvalue = {
+      firstname: 'John',
+      email: 'john@doe.fr',
+      honneypot: 'bot'
+    }
+    cleanDataServiceSpy.cleanObject.and.returnValue(formvalue);
+    component.userForm.setValue(formvalue);
+  
+    spyOn(component.router, 'navigateByUrl');
+    const httpPatchSpy = spyOn(component.http, 'patch');
+    component.onSubmit();
+    
+
+    expect(loaderServiceSpy.show).toHaveBeenCalled();
+    expect(loaderServiceSpy.hide).toHaveBeenCalled();
+  
+    // Vérifie que la requête n'est pas appelée en vérifiant que l'appel est à zéro
+    expect(httpPatchSpy).toHaveBeenCalledTimes(0)
+  
+    expect(component.router.navigateByUrl).toHaveBeenCalledWith('/login');
+  });
+  
+  
+
+  it('devrait soumettre le formulaire si le formulaire est valide', () => {
+    component.userId = '123';
+    component.jwt = 'valid.jwt.token';
+    component.userForm.setValue({
       firstname: 'John',
       email: 'john@doe.fr',
       honneypot: ''
-    }));
-    
-    req.flush({}); // Simule une réponse de succès
+    });
   
-    expect(component.msg).toBe('Modifications réalisées avec succés');
-    expect(component.style_class).toBe('p-3 text-success-emphasis bg-success-subtle border border-success rounded-3');
+    const response = { success: true };
+    cleanDataServiceSpy.cleanObject.and.returnValue(component.userForm.value);
+  
+    const httpSpy = spyOn(component.http, 'patch').and.returnValue(of(response));
+  
+    component.onSubmit();
+    
+    expect(loaderServiceSpy.show).toHaveBeenCalled();
+    expect(loaderServiceSpy.hide).toHaveBeenCalled();
+    expect(component.msg).toBe('Modifications réalisées avec succès');
+    expect(component.style_class).toContain('text-success-emphasis');
+    expect(httpSpy).toHaveBeenCalledWith(
+      `http://127.0.0.1:8000/api/users/${component.userId}`,
+      JSON.stringify(component.userForm.value),
+      { headers: { Authorization: 'Bearer ' + component.jwt, 'Content-Type': 'application/merge-patch+json' } }
+    );
   });
   
-  it('should display an error message if the submission fails', () => {
-    const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU'; // JWT factice
-    localStorage.setItem('jwt', fakeJwt);
+  it('devrait afficher un message en cas de problème lors de la soumission', () => {
     component.userId = '123';
-    component.user_name = 'John';
-    component.user_email = 'john@doe.fr'
-    component.userForm.setValue({ firstname: 'John', email: '', honneypot: '' });
+    component.jwt = 'valid.jwt.token';
+    component.userForm.setValue({
+      firstname: 'John',
+      email: 'john@doe.fr',
+      honneypot: ''
+    });
+  
+    cleanDataServiceSpy.cleanObject.and.returnValue(component.userForm.value);
+  
+    const httpSpy = spyOn(component.http, 'patch').and.returnValue(throwError(() => new Error('Error')));
   
     component.onSubmit();
   
-    const req = httpTestingController.expectOne(`http://127.0.0.1:8000/api/users/123`);
-    expect(req.request.method).toBe('PATCH');
-  
-    req.flush({}, { status: 500, statusText: 'Internal Server Error' });
-  
+    expect(loaderServiceSpy.show).toHaveBeenCalled();
+    expect(loaderServiceSpy.hide).toHaveBeenCalled();
     expect(component.msg).toBe("Une erreur s'est produite. Veuillez réessayer plus tard.");
-    expect(component.style_class).toBe("p-3 text-warning-emphasis bg-warning border border-warning-subtle rounded-3");
+    expect(component.style_class).toContain('text-warning-emphasis');
   });
   
-  it('should navigate to login if the form is invalid', () => {
-    const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU'; // JWT factice
-    localStorage.setItem('jwt', fakeJwt);
-    component.userId = '123';
-    component.user_name = 'John';
-    component.user_email = 'john@doe.fr'
-    component.userForm.setValue({ firstname: '', email: 'invalid-email', honneypot: '' });
   
-    component.onSubmit();
-  
-    expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
-    httpTestingController.expectNone(`http://127.0.0.1:8000/api/users/123`);
-  });
-  
-  //  it('should make HTTP request and handle response when JWT is valid', () => {
-  //   const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU'; // JWT factice
-  //   localStorage.setItem('jwt', fakeJwt);
-    
-  //   // spyOn(localStorage, 'getItem').and.returnValue(fakeJwt);
-  //   const mockResponse = {
-  //     'hydra:member': [
-  //       {
-  //         id: '123',
-  //         firstname: 'John',
-  //         email: 'john@doe.fr'
-  //       }
-  //     ]
-  //   };
-
-  //   // fixture.detectChanges(); // Appelle ngOnInit
-  //   fixture.detectChanges();
-  //   // Attendez la requête GET
-  //   const req = httpTestingController.expectOne(`http://127.0.0.1:8000/api/users?email=john@doe.fr`);
-  //   expect(req.request.method).toBe('GET');
-
-  //   // Simuler la réponse
-  //   req.flush(mockResponse);
-
-  //   // Vérifiez qu'il n'y a pas de requêtes supplémentaires en attente
-  //   // httpTestingController.verify();
-
-  //   // Déclencher la détection des changements après la réponse
-  //   fixture.detectChanges();
-
-  //   // Vérifiez les valeurs traitées par le composant
-  //   expect(component.userId).toEqual('123');
-  //   expect(component.user_name).toEqual('John');
-  //   expect(component.user_email).toEqual('john@doe.fr');
-  // });
-
-  // it('should make HTTP request and handle response when JWT is valid', () => {
-  //   const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5AZG9lLmZyIn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU';
-  //   localStorage.setItem('jwt', fakeJwt);
-  //   spyOn(jwtService, 'decode').and.returnValue({ username: 'john@doe.fr' });
-  
-  //   const mockResponse = {
-  //     'hydra:member': [
-  //       {
-  //         id: '123',
-  //         firstname: 'John',
-  //         email: 'john@doe.fr'
-  //       }
-  //     ]
-  //   };
-  
-  //   // Attendez la requête GET
-  //   const req = httpTestingController.expectOne(`http://127.0.0.1:8000/api/users?email=john@doe.fr`);
-  //   expect(req.request.method).toBe('GET');
-  //   expect(req.request.headers.get('Authorization')).toBe('Bearer ' + fakeJwt);
-
-  //   // Simuler la réponse
-  //   req.flush(mockResponse);
-
-  //   // Déclencher la détection des changements après la réponse
-  //   fixture.detectChanges();
-
-  //   // Vérifiez les valeurs traitées par le composant
-  //   expect(component.userId).toEqual('123');
-  //   expect(component.user_name).toEqual('John');
-  //   expect(component.user_email).toEqual('john@doe.fr');
-  // });
-
-  // it('should load user information if JWT is valid', () => {
-  //   const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJqb2huQGRvZS5mciIsImlhdCI6MTUxNjIzOTAyMn0.H1XaOJUbTFapNjsS0vSPxREkM6KmnaRxoQSnipCq-BU'; // JWT factice
-  //   localStorage.setItem('jwt', fakeJwt);
-  //   const decodedToken = { username: 'john@doe.fr' };
-  //   const mockResponse = {
-  //     'hydra:member': [{ id: '123', firstname: 'john', email: 'john@doe.fr' }]
-  //   };
-    
-  //   spyOn(localStorage, 'getItem').and.returnValue(fakeJwt);
-  //   spyOn(component['jwtService'], 'decode').and.returnValue(decodedToken);
-  //   spyOn(component['http'], 'get').and.returnValue(of(mockResponse));
-  
-  //   component.ngOnInit();
-  
-  //   expect(component.userId).toBe('123');
-  //   expect(component.user_name).toBe('john');
-  //   expect(component.user_email).toBe('john@doe.fr');
-  // });
-
-
 });
+
--- src/app/user-informations/user-informations.component.ts
+++ src/app/user-informations/user-informations.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { jwtDecode } from 'jwt-decode';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { JwtService } from '../services/jwtServices/jwt.service';
+import { CleanDataService } from '../services/cleanDataService/clean-data.service';
+import { LoaderService } from '../services/loaderService/loader.service';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-user-informations',
@@ -15,48 +16,21 @@ import { JwtService } from '../services/jwtServices/jwt.service';
 })
 export class UserInformationsComponent implements OnInit {
 
+  constructor(private CleanDataService: CleanDataService,
+    private loaderService: LoaderService
+  ) {}
+  
   formBuilder: FormBuilder = inject(FormBuilder);
   http: HttpClient = inject(HttpClient);
   router: Router = inject(Router);
-  constructor(private jwtService: JwtService) {}
-  user_information="Mes informations"
-  msg=""
-  style_class=""
-  userId?:string;
-  user_name?:string;
-  user_email?:string;
+
+  user_information="Mes informations";
+  msg = "";
+  style_class ="";
+  userId =""
+  user_name ="";
+  user_email = "";
   jwt:string | null  = localStorage.getItem('jwt');
-
-
-  ngOnInit(): void {
-
-    if (this.jwt?.split('.').length === 3) {
-    const decodedToken: any = this.jwtService.decode(this.jwt);
-    console.log(decodedToken);
-    
-    const username = decodedToken?.username;
-
-    this.http.get(`http://127.0.0.1:8000/api/users?email=${username}`, { headers: { Authorization: 'Bearer ' + this.jwt } })
-    .subscribe({
-      
-      next: (response: any) => {
-      console.log(response);
-      
-      this.userId = response['hydra:member'][0]?.id;
-      this.user_name=response['hydra:member'][0]?.firstname
-      this.user_email=response['hydra:member'][0]?.email;
-
-      console.log(this.userId);
-
-    },
-    error: (err)=>{this.msg="Une erreur s'est produite. Veuillez réessayer plus tard.", this.style_class="p-3 text-warning-emphasis bg-warning border border-warning-subtle rounded-3"}
-    })
-
-    } else{
-      this.router.navigateByUrl('/login');
-    }
-  }
-
   
   userForm  = this.formBuilder.group({
     firstname: [''],
@@ -64,25 +38,64 @@ export class UserInformationsComponent implements OnInit {
     honneypot: [''],
   });
 
-  onSubmit() {
-    let formInputs=this.userForm.value
-    console.log(this.userForm.value);
+  ngOnInit(): void {
+    
+    const encryptData = sessionStorage.getItem('userInfo');
+    if (this.jwt?.split('.').length === 3 && encryptData) {
+      this.loaderService.show();
+      const secretKey = CryptoJS.SHA256(this.jwt).toString();
+      
+      const decryptData = CryptoJS.AES.decrypt(encryptData, secretKey);
+      const userInfos = JSON.parse(decryptData.toString(CryptoJS.enc.Utf8));
+      console.log(userInfos);
 
-    if (this.userForm.valid) {
+      this.userId=userInfos.id;
+      this.user_email=userInfos.email;
+      this.user_name=userInfos.firstname;
+
+      // patchValue permet de définir des valeurs initiale pour ces champs du formulaire
+      this.userForm.patchValue({
+        firstname: this.user_name,
+        email: this.user_email
+    })
+      this.loaderService.hide();
+
+    } else{
+      this.loaderService.hide();
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  
+  onSubmit() {
+    this.loaderService.show();
+    const formInputs = this.CleanDataService.cleanObject(this.userForm.value)
+
+    if (this.userForm.invalid) {
+      this.loaderService.hide()
+    }
+    else if (!formInputs['honneypot'] && formInputs['honneypot'].length==0) {
+
       formInputs['email']= formInputs['email']?formInputs['email'] : this.user_email
       formInputs['firstname']= formInputs['firstname']?formInputs['firstname'] : this.user_name
+
       this.http.patch(`http://127.0.0.1:8000/api/users/${this.userId}`,JSON.stringify(formInputs),{ headers: { Authorization: 'Bearer ' + this.jwt, 'Content-Type': 'application/merge-patch+json' } })
    .subscribe({
      
      next: (response: any) => {
-     console.log(response);
-     this.msg="Modifications réalisées avec succés"
-     this.style_class="p-3 text-success-emphasis bg-success-subtle border border-success rounded-3"     
+      this.loaderService.hide();
+      this.msg="Modifications réalisées avec succès"
+      this.style_class="p-3 text-success-emphasis bg-success-subtle border border-success rounded-3",
+      this.loaderService.hide()     
    },
-   error: (err)=>{this.msg="Une erreur s'est produite. Veuillez réessayer plus tard.", this.style_class="p-3 text-warning-emphasis bg-warning border border-warning-subtle rounded-3"}
+   error: (err)=>{this.msg="Une erreur s'est produite. Veuillez réessayer plus tard.", 
+                  this.style_class="p-3 text-warning-emphasis bg-warning border border-warning-subtle rounded-3",
+                  this.loaderService.hide()
+                }
    })
      
    } else {
+    this.loaderService.hide();
     this.router.navigateByUrl('/login');
    }
     
