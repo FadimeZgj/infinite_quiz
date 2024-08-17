78a002fc867bb3bb22a44ca87dd40090e12b4694
yasmina <el.yasmina.08@gmail.com>
2024-08-17 15:26:20 +0200

    💬 correction d'une faute d'orthographe

--- src/app/change-password/change-password.component.spec.ts
+++ src/app/change-password/change-password.component.spec.ts
@@ -1,30 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { ChangePasswordComponent } from './change-password.component';
-// import { ActivatedRoute } from '@angular/router';
-
-// describe('ChangePasswordComponent', () => {
-//   let component: ChangePasswordComponent;
-//   let fixture: ComponentFixture<ChangePasswordComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [ChangePasswordComponent],
-//       providers: [
-//         { provide: ActivatedRoute, useValue: { snapshot: {} } } // Simulez ActivatedRoute
-//       ],
-//     })
-//     .compileComponents();
-    
-//     fixture = TestBed.createComponent(ChangePasswordComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
 
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ChangePasswordComponent } from './change-password.component';
