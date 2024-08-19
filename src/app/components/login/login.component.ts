8c6e5b2ec9a8a10e84962a0df5f2ed4ab1d9b240
yasmina <el.yasmina.08@gmail.com>
2024-08-19 16:27:52 +0200

    🔥 suppresion de la session

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -55,29 +55,10 @@ export class LoginComponent {
       this.http.post<any>('http://127.0.0.1:8000/api/login_check',JSON.stringify(form), {headers: {'Content-Type': 'application/ld+json' }})
       .subscribe({
         next :  (resp) => {
-          
-        if (resp) {
-          localStorage.setItem('jwt', resp.token);
 
-          this.http.get<any>(`http://127.0.0.1:8000/api/users?email=${form['username']}`, { headers: { Authorization: 'Bearer ' + resp.token } })
-          .subscribe({
+          this.loaderService.hide();
+          this.router.navigateByUrl('/dashboard');
             
-            next: (response: any) => {
-              
-            // création de la clé de décriptage grace à l'algorithme de hashage SHA256 qui fourni un hashe de 32 octet( taille max autorisé pour une clé)
-            // cela permet également de ce défaire des caractères spéciaux pouvant être présent dans le jwt 
-            const secretKey = CryptoJS.SHA256(resp.token).toString();
-            const encryptData = CryptoJS.AES.encrypt(JSON.stringify(response['hydra:member'][0]), secretKey).toString();
-            sessionStorage.setItem('userInfo', encryptData);
-
-            this.loaderService.hide();
-            this.router.navigateByUrl('/dashboard');
-          },
-          error: (err)=>{this.user_msg="Une erreur s'est produite. Veuillez réessayer plus tard.", this.loaderService.hide()}
-          })
-          
-        } 
-  
       },
 
       error: (err)=>{this.user_msg="l'email ou le mot de passe est incorrect",this.loaderService.hide()},
